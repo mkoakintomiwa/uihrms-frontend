3c9df02e84000c41e207b512a2eaa16647ef1af4
mkoakintomiwa <ibnakintomiwa@gmail.com>
2021-12-27 06:16:28 +0100

    Hamdan

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Hamdan!</a>
         </h1>
 
         <p className={styles.description}>
