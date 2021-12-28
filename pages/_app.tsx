42079824c3b8929a7961678bb06f56575d3ad3e9
mkoakintomiwa <ibnakintomiwa@gmail.com>
2021-12-28 21:51:19 +0100

    Commit New

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }: AppProps) {
   return <div>
 
-  		<Script src="/static/topbar/topbar.min.js" strategy="beforeInteractive" />
+  		<Script src="/assets/topbar/topbar.min.js" strategy="beforeInteractive" />
 
 		<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/antd/4.17.4/antd.min.css' integrity='sha512-MIam2KgzIIyxRmfAqAM8+3pvkqhSqtHQVjlFx9v3SwfNPXgB6OW/LHtjWlfj4r5AtgOlLdn2ip7Yb2ndyP7KEQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
 
