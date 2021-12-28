2172c999ddd3e4fe7bd235205698109064f72de9
mkoakintomiwa <ibnakintomiwa@gmail.com>
2021-12-29 00:13:33 +0100

    Added in file system UI logo

--- lib/system/settings.tsx
+++ lib/system/settings.tsx
@@ -1,5 +1,5 @@
 export var portalUrl = "https://uihrms.icitifysolution.com";
 
-export var organizationLogo = "https://uihrms.icitifysolution.com/assets/images/ui-logo.jfif";
+export var organizationLogo = "/assets/images/ui-logo.jfif";
 
 export var organizationLogoAspectRatio = 64/75;
